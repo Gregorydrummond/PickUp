eb458e18b49b0e815d833ab70df369b66240b786
Greg <gregorydrummond@ufl.edu>
2021-07-26 16:42:59 -0400

    Skeleton - Created notification

--- app/src/main/java/com/example/pickup/fragments/mainActivity/HomeFragment.java
+++ app/src/main/java/com/example/pickup/fragments/mainActivity/HomeFragment.java
@@ -1,11 +1,17 @@
 package com.example.pickup.fragments.mainActivity;
 
 import android.app.Activity;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -24,6 +30,7 @@ import android.widget.TextView;
 import com.example.pickup.R;
 import com.example.pickup.activities.Creation;
 import com.example.pickup.activities.GameDetailsActivity;
+import com.example.pickup.activities.MainActivity;
 import com.example.pickup.adapters.HomeFragmentAdapter;
 import com.example.pickup.models.Game;
 import com.mlsdev.animatedrv.AnimatedRecyclerView;
@@ -37,14 +44,13 @@ import org.parceler.Parcels;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
     private static final String TAG = "HomeFragment";
+    private static final String CHANNEL_ID = "New Game Channel";
+
+    NotificationCompat.Builder builder;
+    public static final int notificationId = 3;
 
     Toolbar toolbar;
     static List<Game> gamesFeed;
@@ -68,14 +74,6 @@ public class HomeFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
@@ -96,6 +94,44 @@ public class HomeFragment extends Fragment {
 
         //For toolbar
         setHasOptionsMenu(true);
+
+        //Notifications
+        //Create an explicit intent for an Activity in your app. For notifications
+        Intent intent = new Intent(getContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getContext(), 0, intent, 0);
+
+        //Set notification content
+        builder = new NotificationCompat.Builder(getContext(), CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_basketball_small_icon)
+                .setContentTitle("New Game")
+                .setContentText("New game in your area. Join before it fills up")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        //Create a channel and set the importance
+        createNotificationChannel();
+
+//        //Show notification
+//        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getContext());
+//        notificationManager.notify(notificationId, builder.build());
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name_new_game);
+            String description = getString(R.string.channel_description_new_game);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getActivity().getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     @Override
