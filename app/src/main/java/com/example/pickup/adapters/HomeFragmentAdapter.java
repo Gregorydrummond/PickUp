063839929f18552ac5b5cc693710d21910522ce5
Greg <gregorydrummond@ufl.edu>
2021-08-03 19:29:53 -0400

    Game rating system

--- app/src/main/java/com/example/pickup/adapters/HomeFragmentAdapter.java
+++ app/src/main/java/com/example/pickup/adapters/HomeFragmentAdapter.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,13 +24,21 @@ import com.example.pickup.R;
 import com.example.pickup.activities.GameDetailsActivity;
 import com.example.pickup.models.Game;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseFile;
+import com.parse.ParseQuery;
 import com.parse.ParseUser;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.parceler.Parcels;
 
+import java.util.ArrayList;
 import java.util.List;
+
+//import devlight.io.library.ArcProgressStackView;
 
 public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapter.ViewHolder> {
 
@@ -80,7 +89,7 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapte
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener, View.OnLongClickListener{
 
-        ParseUser user;
+        ParseUser user = ParseUser.getCurrentUser();
         ImageView ivProfilePicture;
         TextView tvUserName;
         TextView tvTimeStamp;
@@ -88,6 +97,8 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapte
         TextView tvGameCapacity;
         TextView tvDistance;
         ImageView ivGamePhoto;
+        ProgressBar gameRatingCircle;
+        TextView tvMatchText;
 
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -98,6 +109,8 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapte
             tvGameCapacity = itemView.findViewById(R.id.tvGameCapacityHome);
             tvDistance = itemView.findViewById(R.id.tvDistanceHome);
             ivGamePhoto = itemView.findViewById(R.id.ivGamePhotoHome);
+            gameRatingCircle = itemView.findViewById(R.id.pbGameRating);
+            tvMatchText = itemView.findViewById(R.id.tvMatchText);
 
             //Add this as the itemView's OnClickListener
             itemView.setOnClickListener(this);
@@ -105,7 +118,6 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapte
         }
 
         public void bind(Game game) throws ParseException {
-            user = ParseUser.getCurrentUser();
             ParseUser creator = game.getCreator().fetchIfNeeded();
             //Set data
             ParseFile profilePicture = creator.getParseFile("profilePicture");
@@ -126,13 +138,196 @@ public class HomeFragmentAdapter extends RecyclerView.Adapter<HomeFragmentAdapte
             tvUserName.setText(textName);
             String textTime = "· " + game.getCreatedAtDate();
             tvTimeStamp.setText(textTime);
-            String textGameType = "Game Type: " + game.getGameType();
+            String textGameType = game.getGameType();
             tvGameType.setText(textGameType);
             int playerCount = game.getPlayerCount();
             String textGameCapacity = "Players: " + playerCount + "/" + game.getPlayerLimit();
             tvGameCapacity.setText(textGameCapacity);
             String textDistance = String.format("%.2f", user.getParseGeoPoint("playerLocation").distanceInMilesTo(game.getLocation())) + " miles away";
             tvDistance.setText(textDistance);
+
+            getPlayersData(game);
+        }
+
+        private void getPlayersData(Game game) {
+            List<JSONArray> statArray = new ArrayList<>();
+            Log.i(TAG, "getPlayersData: Querying players");
+            //Which class we are querying
+            ParseQuery<ParseUser> query = ParseQuery.getQuery(ParseUser.class);
+
+            //Constraints
+            query.whereEqualTo("currentGame", game);
+
+            //Get players
+            query.findInBackground((players, e) -> {
+                if(e == null) {
+                    //Log.i(TAG, "done: " + players);
+                    for(ParseUser player : players) {
+                        statArray.add(player.getJSONArray("stats"));
+                    }
+                    try {
+                        createRating(statArray, game, players);
+                    } catch (JSONException jsonException) {
+                        Log.e(TAG, "getPlayersData: Error creating game ratings", e);
+                    }
+                }
+                else {
+                    Log.e(TAG, "getPlayersData: Error getting players", e);
+                }
+            });
+        }
+
+        private void createRating(List<JSONArray> statArray, Game game, List<ParseUser> players) throws JSONException {
+            String gameType = game.getGameType();
+
+            //Get user's data
+            JSONArray usersStats = user.getJSONArray("stats");
+            int userTotalPointsOfThisType = 0, userTotalXPOfThisType = 0, userTotalGamesPlayedOfThisType = 0, userTotalGamesWonOfThisType = 0, userTotalMostPointsScoredOfThisType = 0;
+            int userStreak = user.getInt("currentStreak");
+
+            //User stats data
+            for(int i = 0; i < usersStats.length(); i++) {
+                JSONObject specificGameTypeStatsObject = usersStats.getJSONObject(i);
+
+                if(specificGameTypeStatsObject.getString("gameType").equals(gameType)) {
+                    //Total points
+                    int pointsOfThisType = specificGameTypeStatsObject.getInt("totalPoints");
+                    userTotalPointsOfThisType += pointsOfThisType;
+
+                    //Total xp of this type
+                    int xpOfThisType = specificGameTypeStatsObject.getInt("totalXP");
+                    userTotalXPOfThisType += xpOfThisType;
+
+                    //Total games played of this type
+                    int gamesPlayedOfThisType = specificGameTypeStatsObject.getInt("gamesPlayed");
+                    userTotalGamesPlayedOfThisType += gamesPlayedOfThisType;
+
+                    //Total games won of this type
+                    int gamesWonOfThisType = specificGameTypeStatsObject.getInt("gamesWon");
+                    userTotalGamesWonOfThisType += gamesWonOfThisType;
+
+                    //Most points scored of this type
+                    int mostPointsScoredOfThisTypes = specificGameTypeStatsObject.getInt("mostPoints");
+                    userTotalMostPointsScoredOfThisType += mostPointsScoredOfThisTypes;
+
+                    break;
+                }
+            }
+
+            //Get players' data
+            double playerCount = (double) game.getPlayerCount();
+            int totalXP = 0, totalXPOfThisType = 0, totalPoints = 0, totalPointsOfThisType = 0, totalGamesPlayed = 0, totalGamesPlayedOfThisType = 0;
+            int totalGamesWon = 0, totalGamesWonOfThisType = 0, totalMostPointsScoredOfThisType = 0;
+            double distance = user.getParseGeoPoint("playerLocation").distanceInMilesTo(game.getLocation());
+            double maxDistance = user.getDouble("maxDistance");
+            int streak = 0;
+            double averagePointsOfThisType = 0, averageXPOfThisType = 0, averageGamePlayedOfThisType = 0, averageGamesWonOfThisType = 0, averageMostPointsScoredOfThisType = 0, averageStreakOfThisType = 0;
+
+            //Streaks
+            for(ParseUser player : players) {
+                streak += player.getInt("currentStreak");
+            }
+
+            //Stats data
+            for(int i = 0; i < statArray.size(); i++) {
+                JSONArray jsonArray = statArray.get(i);
+                JSONObject allStatsObject = jsonArray.getJSONObject(0);
+
+                //Total Points
+                int points = allStatsObject.getInt("totalPoints");
+                totalPoints += points;
+
+                //Total XP
+                int xp = allStatsObject.getInt("totalXP");
+                totalXP += xp;
+
+                //Total games played
+                int gamesPlayed = allStatsObject.getInt("gamesPlayed");
+                totalGamesPlayed += gamesPlayed;
+
+                //Total games won
+                int gamesWon = allStatsObject.getInt("gamesWon");
+                totalGamesWon += gamesWon;
+
+                //Specific game types
+                for (int j = 0; j < jsonArray.length(); j++) {
+                    JSONObject specificGameTypeStatsObject = jsonArray.getJSONObject(j);
+
+                    if(specificGameTypeStatsObject.getString("gameType").equals(gameType)) {
+                        //Total points
+                        int pointsOfThisType = specificGameTypeStatsObject.getInt("totalPoints");
+                        totalPointsOfThisType += pointsOfThisType;
+
+                        //Total xp of this type
+                        int xpOfThisType = specificGameTypeStatsObject.getInt("totalXP");
+                        totalXPOfThisType += xpOfThisType;
+
+                        //Total games played of this type
+                        int gamesPlayedOfThisType = specificGameTypeStatsObject.getInt("gamesPlayed");
+                        totalGamesPlayedOfThisType += gamesPlayedOfThisType;
+
+                        //Total games won of this type
+                        int gamesWonOfThisType = specificGameTypeStatsObject.getInt("gamesWon");
+                        totalGamesWonOfThisType += gamesWonOfThisType;
+
+                        //Most points scored of this type
+                        int mostPointsScoredOfThisTypes = specificGameTypeStatsObject.getInt("mostPoints");
+                        totalMostPointsScoredOfThisType += mostPointsScoredOfThisTypes;
+
+                        break;
+                    }
+                }
+            }
+
+            //Averages
+            if (playerCount != 0) {
+                averagePointsOfThisType = totalPointsOfThisType / playerCount;
+                averageXPOfThisType = totalXPOfThisType / playerCount;
+                averageGamePlayedOfThisType = totalGamesPlayedOfThisType / playerCount;
+                averageGamesWonOfThisType = totalGamesWonOfThisType / playerCount;
+                averageMostPointsScoredOfThisType = totalMostPointsScoredOfThisType / playerCount;
+                averageStreakOfThisType = streak / playerCount;
+            }
+
+            //Percent errors
+            double pointsPE = signedPercentError(userTotalPointsOfThisType, averageMostPointsScoredOfThisType);
+            double xpPE = signedPercentError(userTotalXPOfThisType, averageXPOfThisType);
+            double gamesPlayedPE = signedPercentError(userTotalGamesPlayedOfThisType, averageGamePlayedOfThisType);
+            double gamesWonPE = signedPercentError(userTotalGamesWonOfThisType, averageGamesWonOfThisType);
+            double mostPointsScoredPE = signedPercentError(userTotalMostPointsScoredOfThisType, averageMostPointsScoredOfThisType);
+            double streakPE = signedPercentError(userStreak, averageStreakOfThisType);
+            double distancePE = signedPercentError(maxDistance - distance, maxDistance);
+
+            Log.i(TAG, "createRating:\nAverage Points: " + averagePointsOfThisType + "\n Average XP: " + averageXPOfThisType
+            + "\n Average Games Played: " + averageGamePlayedOfThisType + "\n Average Games Won: " + averageGamesWonOfThisType
+            + "\n Average Most Points Scored: " + averageMostPointsScoredOfThisType + "\n Average Streak: " + averageStreakOfThisType);
+
+            Log.i(TAG, "createRating:\nPoints PE: " + pointsPE + "\nXP PE: " + xpPE
+                    + "\nGames Played PE: " + gamesPlayedPE + "\nGames Won PE: " + gamesWonPE
+                    + "\nMost Points Scored PE: " + mostPointsScoredPE + "\nStreak PE: " + streakPE
+                    + "\nDistance PE: " + distancePE);
+
+            //Calculate Ratings
+            double weightedPESum = (0.25 * pointsPE) + (0.25 * distancePE) + (0.20 * xpPE) + (0.15 * gamesWonPE) + (0.05 * gamesPlayedPE) + (0.05 * streakPE) + (0.05 * mostPointsScoredPE);
+            double peSum = pointsPE + xpPE + gamesWonPE + gamesPlayedPE + streakPE + distancePE + mostPointsScoredPE;
+            double weightedPEAverage = (weightedPESum/peSum) * 100;
+            double rating = 100 - weightedPEAverage;
+
+            Log.d(TAG, "createRating: Weighted Average of PE: " + weightedPEAverage);
+            Log.d(TAG, "createRating: Rating: " + rating);
+
+            //Rating circle
+            int roundedRating = (int) Math.round(rating);
+            gameRatingCircle.setProgress(roundedRating);
+            String matchText = roundedRating + "% Match";
+            tvMatchText.setText(matchText);
+        }
+
+        private double signedPercentError(double actual, double expected) {
+            if(expected == 0) {
+                return 0;
+            }
+            return Math.abs(1 - (Math.abs((actual - expected) / expected )) * 100);
         }
 
         @Override
